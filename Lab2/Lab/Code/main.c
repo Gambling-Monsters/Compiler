76470008a3c2edf53584fc9df4b561b2d1d48d46
Wang Junrui <davidwang20000710@gmail.com>
2021-04-17 09:33:40 +0800

    调整main文件

--- Lab2/Lab/Code/main.c
+++ Lab2/Lab/Code/main.c
@@ -1,22 +1,31 @@
-#include "lex.yy.c"
-#include "syntax.y"
 #include <stdio.h>
-#include <stdarg.h>
-int main(int argc, char** argv){
-	if(argc <= 1)
-			return 1;
-	FILE *f = fopen(argv[1], "r");
-	if(!f){
-		perror(argv[1]);
-		return 1;
-	}
-    yylineno=1;
-	yyrestart(f);
-	yyparse();
-	if(!lexError){
-        if(!synErr){
-            func(root,0);
-        }
-	}
-	return 0;
-}
+#include "syntax.tab.h"
+extern FILE *yyin;
+extern int yydebug;
+extern int lexError;
+extern int synErr;
+extern struct AST_Node *root;
+
+int yylex();
+void func(struct AST_Node* s_node,int h);
+void yyrestart(FILE *f);
+
+int main(int argc, char **argv)
+{
+
+    if (argc <= 1)
+        return 1;
+    FILE *f = fopen(argv[1], "r");
+    if (!f)
+    {
+        perror(argv[1]);
+        return 1;
+    }
+    yyrestart(f);
+    yyparse();
+    if (lexError == 0 && synErr == 0)
+    {
+        func(root, 0);
+    }
+    return 0;
+};
